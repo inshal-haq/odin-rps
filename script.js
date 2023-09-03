bbb62e228ca10acb7870ec0cbbb177f60a6f9eab
Inshal Haq <inshalulhaq11@gmail.com>
2023-09-03 13:03:50 -0500

    Add capitalize() for capitalizing player and computer choices

--- script.js
+++ script.js
@@ -5,30 +5,30 @@ function playGame(playerSelection, computerSelection) {
     return 'Tie Game!';
   }
 
-  if (playerSelection === 'Rock') {
-    if (computerSelection === 'Scissors') {
+  if (playerSelection === 'rock') {
+    if (computerSelection === 'scissors') {
       playerWon = true;
-    } else if (computerSelection === 'Paper') {
+    } else if (computerSelection === 'paper') {
       playerWon = false;
     }
-  } else if (playerSelection === 'Scissors') {
-    if (computerSelection === 'Paper') {
+  } else if (playerSelection === 'scissors') {
+    if (computerSelection === 'paper') {
       playerWon = true;
-    } else if (computerSelection === 'Rock') {
+    } else if (computerSelection === 'rock') {
       playerWon = false;
     }
-  } else if (playerSelection === 'Paper') {
-    if (computerSelection === 'Rock') {
+  } else if (playerSelection === 'paper') {
+    if (computerSelection === 'rock') {
       playerWon = true;
-    } else if (computerSelection === 'Scissors') {
+    } else if (computerSelection === 'scissors') {
       playerWon = false;
     }
   }
   
   if (playerWon) {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+    return `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
   } else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    return `You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`;
   }
 }
 
@@ -37,10 +37,16 @@ function getComputerChoice() {
 
   switch (choice) {
     case 1: 
-      return 'Rock';
+      return 'rock';
     case 2: 
-      return 'Paper';
+      return 'paper';
     case 3: 
-      return 'Scissors';
+      return 'scissors';
   }
+}
+
+function capitalize(text) {
+  const firstLetter = text.substr(0,1).toUpperCase();
+  const rest = text.substr(1).toLowerCase();
+  return firstLetter + rest;
 }
