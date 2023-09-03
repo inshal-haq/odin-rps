3550ee37a65356979a0b7cb3a21455c82ddfc93e
Inshal Haq <inshalulhaq11@gmail.com>
2023-09-03 12:25:26 -0500

    Add playGame() for determining game result

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+function playGame(playerSelection, computerSelection) {
+  let playerWon = false;
+  
+  if (playerSelection === computerSelection) {
+    return 'Tie Game!';
+  }
+
+  if (playerSelection === 'Rock') {
+    if (computerSelection === 'Scissors') {
+      playerWon = true;
+    } else if (computerSelection === 'Paper') {
+      playerWon = false;
+    }
+  } else if (playerSelection === 'Scissors') {
+    if (computerSelection === 'Paper') {
+      playerWon = true;
+    } else if (computerSelection === 'Rock') {
+      playerWon = false;
+    }
+  } else if (playerSelection === 'Paper') {
+    if (computerSelection === 'Rock') {
+      playerWon = true;
+    } else if (computerSelection === 'Scissors') {
+      playerWon = false;
+    }
+  }
+  
+  if (playerWon) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
 function getComputerChoice() {
   const choice = Math.floor((Math.random() * 3) + 1);
 
