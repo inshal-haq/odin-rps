bd01f602c30456ef1e108663ef855a351eb66a62
Inshal Haq <inshalulhaq11@gmail.com>
2023-09-06 12:54:49 -0500

    Refactor RPS to include button selection
    Lots of updates here as I forgot to consistently commit.
    HTML
      - Add rps buttons
      - Add reset button to reset score
      - Add result div to display round winner
      - Add score div to display wins, losses, and ties
      - Add winner div to display game winner
    
    JS
      - Get selection from rps button click
      - Show score, and update score after round winner chosen
      - Show winner when 5 wins or 5 losses met
      - Remove capitalize() since button allows for specifying rps text

--- script.js
+++ script.js
@@ -1,47 +1,94 @@
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const resetBtn = document.querySelector('#reset');
+
+const resultDiv = document.querySelector('#result');
+const scoreDiv = document.querySelector('#score');
+const winnerDiv = document.querySelector('#winner');
+
+let score = {
+  wins: 0,
+  losses: 0,
+  ties: 0
+};
+
+updateScore();
 game();
 
 function game() {
-  for (let i = 1; i <= 5; i++) {
-    let playerChoice = prompt('Please input your Move (rock, paper, scissors):');
-    if (!playerChoice) break;
-    
-    playerChoice = playerChoice.toLowerCase();
-    console.log(playRound(playerChoice, getComputerChoice()));
-  }
+  rockBtn.addEventListener('click', () => {
+    resultDiv.textContent = playRound('Rock', getComputerChoice());
+  });
+  
+  paperBtn.addEventListener('click', () => {
+    resultDiv.textContent = playRound('Paper', getComputerChoice());
+  });
+  
+  scissorsBtn.addEventListener('click', () => {
+    resultDiv.textContent = playRound('Scissors', getComputerChoice());
+  });
+
+  resetBtn.addEventListener('click', () => {
+    score = {
+      wins: 0,
+      losses: 0,
+      ties: 0
+    };
+
+    updateScore();
+    winnerDiv.textContent = '';
+  });
 }
 
 function playRound(playerMove, computerMove) {
   let playerWon = false;
 
   if (playerMove === computerMove) {
+    score.ties++;
+    updateScore();
     return 'Tie Game!';
   }
 
-  if (playerMove === 'rock') {
-    if (computerMove === 'scissors') {
+  if (playerMove === 'Rock') {
+    if (computerMove === 'Scissors') {
       playerWon = true;
-    } else if (computerMove === 'paper') {
+    } else if (computerMove === 'Paper') {
       playerWon = false;
     }
 
-  } else if (playerMove === 'scissors') {
-    if (computerMove === 'paper') {
+  } else if (playerMove === 'Scissors') {
+    if (computerMove === 'Paper') {
       playerWon = true;
-    } else if (computerMove === 'rock') {
+    } else if (computerMove === 'Rock') {
       playerWon = false;
     }
 
-  } else if (playerMove === 'paper') {
-    if (computerMove === 'rock') {
+  } else if (playerMove === 'Paper') {
+    if (computerMove === 'Rock') {
       playerWon = true;
-    } else if (computerMove === 'scissors') {
+    } else if (computerMove === 'Scissors') {
       playerWon = false;
     }
   }
+
+  if (playerWon) {
+    score.wins++;
+  } else {
+    score.losses++;
+  }
+
+  updateScore();
   
-  return (playerMove) ?
-    `You Win! ${capitalize(playerMove)} beats ${capitalize(computerMove)}` :
-    `You Lose! ${capitalize(computerMove)} beats ${capitalize(playerMove)}`;
+  if (score.wins === 5) {
+    winnerDiv.textContent = 'You Win!';
+  } else if (score.losses === 5) {
+    winnerDiv.textContent = 'Computer Wins';
+  }
+  
+  return (playerWon) ?
+    `You Win! ${playerMove} beats ${computerMove}` :
+    `You Lose! ${computerMove} beats ${playerMove}`;
 }
 
 function getComputerChoice() {
@@ -49,16 +96,15 @@ function getComputerChoice() {
 
   switch (choice) {
     case 1: 
-      return 'rock';
+      return 'Rock';
     case 2: 
-      return 'paper';
+      return 'Paper';
     case 3: 
-      return 'scissors';
+      return 'Scissors';
   }
 }
 
-function capitalize(text) {
-  const firstLetter = text.substr(0,1).toUpperCase();
-  const rest = text.substr(1).toLowerCase();
-  return firstLetter + rest;
+function updateScore() {
+  scoreDiv.textContent = 
+    `Wins: ${score.wins} | Losses: ${score.losses} | Ties: ${score.ties}`;
 }
